3b4ed5486c447aa5122c4d03274dff0de961293e
Baban <sinha.baban@gmail.com>
2021-08-02 23:55:02 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/home_page.dart';
+import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:flutter_tutorial/pages/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -20,6 +21,16 @@ class MyApp extends StatelessWidget {
     const double pi1 = 3.14; //We can't change value
     final String list = ""; //We can chage value*/
 
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      //home: HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => HomePage(),
+      },
+    );
   }
 }
